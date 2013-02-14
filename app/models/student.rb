f4a18a137c9f365730e300fcd22a5c433740fc80
Apprentice <apprentice@dbc25.local>
2013-02-14 13:45:58 -0800

    Added phone_format_cleanup method, passed advanced validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,8 @@ class Student < ActiveRecord::Base
   validates :age, :numericality => {:greater_than => 4}
   validates :email, :uniqueness => true, :format => {
     :with => /\w+@\w+.\w{2,}/, :message => "Unacceptable format"}
+  # validates :phone, :format => { :with => /\d{10,}[^\D]/, :message => "Unacceptable format"}
+  validates :phone, :length => {:minimum => 10}
 
   def name
     self.first_name + " " + self.last_name
@@ -16,6 +18,11 @@ class Student < ActiveRecord::Base
     now = Date.today
     age = now.year - self.birthday.year - ((now.month > self.birthday.month || 
           (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end  
+
+  def phone_format_cleanup
+    #update phone in the db with the cleaned up version and only run if phone is not nil
+    self.phone = self.phone.gsub(/([-() ])/, '') if self.phone != nil
   end  
 
 end
