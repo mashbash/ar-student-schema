f6b5628ffeca6efe2a0e63fb85ee4379f620b78e
Apprentice <apprentice@dbc25.local>
2013-02-14 12:06:50 -0800

    updated _create_students.rb file, updated student.rb, passed student_spec file

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+
+  
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    #copied this from the spec file
+    now = Date.today
+    age = now.year - self.birthday.year - ((now.month > self.birthday.month || 
+          (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end  
+
+end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,18 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table :students do |table|
+      table.column :first_name, :string
+      table.column :last_name, :string
+      table.column :gender, :string
+      table.column :age, :integer
+      table.column :birthday, :date
+      table.column :email, :string
+      table.column :phone, :string
+    end
+
+    #can create many tables in here
+
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
